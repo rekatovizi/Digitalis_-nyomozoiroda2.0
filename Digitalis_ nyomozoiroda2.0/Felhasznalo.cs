7b80f2bff2900138caf69f46212ef81f315016a8
Réka Tóvizi <tovizi.reka@petrik.hu>
2026-02-19 09:57:10 +0100

    Add project files.

---
+++ Digitalis_ nyomozoiroda2.0/Felhasznalo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digitalis__nyomozoiroda2._0
+{
+    internal class Felhasznalo
+    {
+        string nev;
+        int azonosito;
+        string szerepkor;
+
+        public Felhasznalo(string nev, int azonosito, string szerepkor)
+        {
+            this.nev = nev;
+            this.azonosito = azonosito;
+            this.szerepkor = szerepkor;
+        }
+
+        public string Nev { get => nev; set => nev = value; }
+        public int Azonosito { get => azonosito; set => azonosito = value; }
+        public string Szerepkor { get => szerepkor; set => szerepkor = value; }
+    }
+}
---
+++ Digitalis_ nyomozoiroda2.0/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Digitalis__nyomozoiroda2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
