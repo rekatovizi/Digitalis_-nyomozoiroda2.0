ac9db5b7ad757705209b41df7e688603953432ed
Réka Tóvizi <tovizi.reka@petrik.hu>
2026-02-19 10:03:52 +0100

    gyanusitott

---
+++ Digitalis_ nyomozoiroda2.0/Gyanusitott.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digitalis__nyomozoiroda2._0
+{
+    internal class Gyanusitott
+    {
+        Szemely szemely;
+        int gyanusitotsagiszint;
+        string statusz;
+
+        public Gyanusitott(Szemely szemely, int gyanusitotsagiszint, string statusz)
+        {
+            this.szemely = szemely;
+            this.gyanusitotsagiszint = gyanusitotsagiszint;
+            this.statusz = statusz;
+        }
+
+        public int Gyanusitotsagiszint { get => gyanusitotsagiszint; set => gyanusitotsagiszint = value; }
+        public string Statusz { get => statusz; set => statusz = value; }
+        internal Szemely Szemely { get => szemely; set => szemely = value; }
+    }
+}
