2603f5abd64802884f35f13a53a413ad0bcec691
Réka Tóvizi <tovizi.reka@petrik.hu>
2026-02-19 10:05:14 +0100

    tanu

---
+++ Digitalis_ nyomozoiroda2.0/Tanu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digitalis__nyomozoiroda2._0
+{
+    internal class Tanu
+    {
+        Szemely szemely;
+        string vallomas;
+        DateTime vallomasIdeje;
+        public Tanu(Szemely szemely, string vallomas, DateTime vallomasIdeje    )
+        {
+            this.szemely = szemely;
+            this.vallomas = vallomas;
+            this.vallomasIdeje = vallomasIdeje;
+        }
+        public string Vallomas { get => vallomas; set => vallomas = value; }
+        public DateTime VallomasIdeje { get => vallomasIdeje; set => vallomasIdeje = value; }
+        internal Szemely Szemely { get => szemely; set => szemely = value; }
+    }
+}
