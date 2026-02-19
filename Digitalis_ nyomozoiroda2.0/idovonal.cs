cc150b236b0b5924b3ee99f37ec7d0fdbe8ea6ed
Réka Tóvizi <tovizi.reka@petrik.hu>
2026-02-19 10:09:55 +0100

    idővonal

---
+++ Digitalis_ nyomozoiroda2.0/idovonal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digitalis__nyomozoiroda2._0
+{
+    internal class Idovonal
+    {
+        DateTime DateTime;
+        string esemeny;
+
+        public Idovonal(DateTime dateTime, string esemeny)
+        {
+            DateTime = dateTime;
+            this.esemeny = esemeny;
+        }
+
+        public DateTime DateTime1 { get => DateTime; set => DateTime = value; }
+        public string Esemeny { get => esemeny; set => esemeny = value; }
+    }
+}
