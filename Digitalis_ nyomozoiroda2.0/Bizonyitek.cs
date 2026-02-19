06403816268370876bd0b0a4154653cc9f8b916e
Réka Tóvizi <tovizi.reka@petrik.hu>
2026-02-19 10:07:28 +0100

    bizonyitek

---
+++ Digitalis_ nyomozoiroda2.0/Bizonyitek.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digitalis__nyomozoiroda2._0
+{
+    internal class Bizonyitek
+    {
+        int bizonyitekAzonosito;
+        string tipus;
+        string leiras;
+        int megbizhatosagiszint;
+
+        public Bizonyitek(int bizonyitekAzonosito, string tipus, string leiras, int megbizhatosagiszint)
+        {
+            this.bizonyitekAzonosito = bizonyitekAzonosito;
+            this.tipus = tipus;
+            this.leiras = leiras;
+            this.megbizhatosagiszint = megbizhatosagiszint;
+        }
+
+        public int BizonyitekAzonosito { get => bizonyitekAzonosito; set => bizonyitekAzonosito = value; }
+        public string Tipus { get => tipus; set => tipus = value; }
+        public string Leiras { get => leiras; set => leiras = value; }
+        public int Megbizhatosagiszint { get => megbizhatosagiszint; set => megbizhatosagiszint = value; }
+    }
+}
