82c5e31564a5184b368cc8a001318664ed412672
Réka Tóvizi <tovizi.reka@petrik.hu>
2026-02-19 10:25:01 +0100

    bizonyítekkezelo

---
+++ Digitalis_ nyomozoiroda2.0/Bizonyitekkezelo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digitalis__nyomozoiroda2._0
+{
+    internal class Bizonyitekkezelo
+    {
+        List<Bizonyitek> bizonyitekok;
+        public Bizonyitekkezelo()
+        {
+            bizonyitekok = new List<Bizonyitek>();
+        }
+        public void BizonyitekHozzaadasa(Bizonyitek bizonyitek)
+        {
+            bizonyitekok.Add(bizonyitek);
+        }
+        public void BizonyitekTorlese(Bizonyitek bizonyitek)
+        {
+            bizonyitekok.Remove(bizonyitek);
+        }
+        public List<Bizonyitek> BizonyitekekLekerdezese()
+        {
+            return bizonyitekok;
+        }
+    }
+}
